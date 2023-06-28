35222666839145821043a986f4d21ba8d4bec6fe
Laura Cristina <lauracristina.s@hotmail.com>
2023-06-28 15:54:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Fragment } from 'react';
+import Header from './componentes/header/Header';
+import Destaques from './componentes/main/sessaoProdutos/Destaques';
+
+
+
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+      <Destaques/>
+    </Fragment>
+  )
+
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './css/style.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    
+    <App />
+    
+)
