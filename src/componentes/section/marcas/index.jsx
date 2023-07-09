1a06872579dbf832d8bbdccf0c71a247f63a5161
Laura Cristina <lauracristina.s@hotmail.com>
2023-07-09 18:34:26 -0300

    colocando a chamada em 100viewport

--- src/componentes/section/marcas/index.jsx
+++ src/componentes/section/marcas/index.jsx
@@ -1,44 +1,12 @@
 import '@/componentes/section/marcas/marcas.css'
-import Destaque1 from '@/img/destaque1.png';
-import Destaque2 from '@/img/destaque2.png';
-import Destaque3 from '@/img/destaque3.png';
-import Titulo from "@/componentes/commons/Titulo";
-import Titulo2 from "@/componentes/commons/Titulo2";
-import Tenis from "@/img/tenis_grande.png"
 
-const produtos = [
-    {
-        title: 'Basketball',
-        img: Destaque1
-    },
-    {
-        title: 'LifeStyle',
-        img: Destaque2
-    }, {
-        title: 'Performance',
-        img: Destaque3
-    },
-]
 
-export default function Marcas() {
-    return (
-        <section>
-            <div className="marcas-container">
-              marcas marcas marcas 
-              </div>
+export default function Marcas(){
+    return(
+        <section id="marcas">
+            <div className="marcas">
+              <img src="./img/marcas.png" alt="marcas que trabalhamos" />
+            </div>
         </section>
-        // <section>
-        //     <div className="destaques-container borda">
-        //         {produtos.map((produto) => (
-        //             <div key={produto.title} className='container'>
-        //                 <div>
-        //                     <div className='titulosDestaques'>{produto.title}</div>
-        //                     <img className='imgDestaques' src={produto.img} alt={produto.title} />
-        //                 </div>
-        //             </div>
-        //         ))}
-        //     </div>
-        // </section>
-    );
+    )
 }
-
