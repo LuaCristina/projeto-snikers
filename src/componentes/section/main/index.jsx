6747a1858021741b2ea1c928669207e9a17eec3d
Laura Cristina <lauracristina.s@hotmail.com>
2023-07-09 16:49:28 -0300

    responsividade da chamada

--- src/componentes/section/main/index.jsx
+++ src/componentes/section/main/index.jsx
@@ -1,24 +1,32 @@
 import '@/componentes/section/main/main.css'
 
 const Index = (props) => {
-    return (
-        <section>
-            <div className="principal sectionMarge">
-                <div className="container">
-                    {/* Conteúdo do contêiner */}
-                    <div>
+    return ( 
+        <div className="principal">
+            <div className="background-container">
+                <video className="video-container" playsInline autoPlay loop muted>
+                    <source type="video/webm" src={props.data.background.video}/>
+                    <img className="image-container" src={props.data.background.image}/>
+                </video>
+            </div>
+            
+            <div className="area">
+                {/* Conteúdo do contêiner */}
+                <div className="left-column"> 
+                    <div className="titulos">
                         <h1 id="tituloPrincipal">{props.data.title}</h1>
-                    </div>
-                    <div>
-                        <div id="bola"></div>
-                        <img className="tenis" src={props.data.image} />
-                    </div>
-                    <div>
                         <h2 id="subtitulo">{props.data.subtitle}</h2>
                     </div>
                 </div>
+
+                <div className="right-column">
+                    <div className="imagemPrincipal">
+                        <div id="bola"></div>
+                        <img className="tenis" src={props.data.image} />
+                    </div>
+                </div>
             </div>
-        </section>
+        </div> 
     );
 }
 
