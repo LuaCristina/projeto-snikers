36a1c8024ab56300d43adc084aceffd2f4f91d1e
Laura Cristina <lauracristina.s@hotmail.com>
2023-07-10 18:38:56 -0300

    editando sessao testemonials

--- src/componentes/section/testemonials/index.jsx
+++ src/componentes/section/testemonials/index.jsx
@@ -2,18 +2,17 @@ import '@/componentes/section/testemonials/testemonials.css'
 import React from 'react';
 
 const Testimonials = (props) => {
-  return (
-    <div className="container">
-        console.log(props)
-      {props.data.map((testimonial, index) => (
-        <div className="testimonial" key={index}>
-          <h3>{testimonial.name}</h3>
-          <p>{testimonial.content}</p>
-          <div className="rate">{testimonial.rate} estrelas</div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Testimonials;
+    return (
+      <div className="testimonials-container">
+        {props.data.map((testimonial, index) => (
+          <div className="testimonial" key={index}>
+            <h3>{testimonial.name}</h3>
+            <p>{testimonial.content}</p>
+            <div className="rate">{testimonial.rate} estrelas</div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+  export default Testimonials;
