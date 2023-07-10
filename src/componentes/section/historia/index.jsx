6e9506960233aa76b683802f6f8bd6af85934a33
Laura Cristina <lauracristina.s@hotmail.com>
2023-07-10 11:40:39 -0300

    atualizaçao JSON historico

--- src/componentes/section/historia/index.jsx
+++ src/componentes/section/historia/index.jsx
@@ -1,33 +1,28 @@
 import '@/componentes/section/historia/historia.css'
-
-
-
 const Historia = (props) => {
     return (
-        <section>
+      <section>
         <div className="areaPrincipalHistoria">
-            {/* Conteúdo do contêiner */}
-            {props.data.map((item, index) => (
-                <div className="columns-container" key={index}>
-                    <div className="esquerda-column">
-                        <img className="jordan" src={item.background.image} />
-                    </div>
-                    <div className="direita-column">
-                        <h1 className="tituloPrincipal">{item.title}</h1>
-                        <p className="texto-jordan">{item.content}</p>
-                        <div className="outra-div">
-                            <div className="div1"></div>
-                            <div className="div2"></div>
-                        </div>
-                    </div>
+          {/* Conteúdo do contêiner */}
+          {props.data.map((item, index) => (
+            <div className="columns-container" key={index}>
+              <div className="esquerda-column">
+                <img className="jordan" src={item.background.image} alt="" />
+              </div>
+              <div className="direita-column">
+                <h1 className="tituloPrincipal">{item.title}</h1>
+                <p className="texto-jordan">{item.content}</p>
+                <div className="outra-div">
+                  {/* <div className="tenis2"><img  src={item.itens.image} alt="" /></div> */}
+                  {/* <div className="div2"><p>{item.itens.text}</p></div> */}
                 </div>
-            ))}
+              </div>
+            </div>
+          ))}
         </div>
-    </section>
-    
-
+      </section>
     );
-};
-
-
-export default Historia;
+  };
+  
+  export default Historia;
+  
