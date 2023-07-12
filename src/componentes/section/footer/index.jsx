1d357da8353aba5b1c9d48bd60871d85db8a2134
Laura Cristina <lauracristina.s@hotmail.com>
2023-07-12 14:46:14 -0300

    correçao css footer

--- src/componentes/section/footer/index.jsx
+++ src/componentes/section/footer/index.jsx
@@ -3,21 +3,25 @@ import React from 'react';
 
 const Footer = (props) => {
     return (
-      <div className="footer">
-        <div className="footer-title">
-          <h1>{props.data.title}</h1>
-          <img src={props.data.image} alt="Logo" />
+        <div className="footer">
+            <div className="footer-title">
+                <div className="title-wrapper">
+                    <h1>{props.data.title}</h1>
+                </div>
+                <div className="image-wrapper">
+                    <img src={props.data.image} alt="Logo" />
+                </div>
+            </div>
+            <div className='marcas-logos-area'>
+                    {props.data.socias.map((social, index) => 
+                       <a key={index} href={social.url}>
+                        <img src={social.image} className='marcas-logo' alt="Social" />
+                    </a>
+                    )} 
+                </div>
+        
         </div>
-        <div className="footer-socials">
-          {props.data.socias.map((social, index) => (
-            <a key={index} href={social.url}>
-              <img src={social.image} alt="Social" />
-            </a>
-          ))}
-        </div>
-      </div>
     );
-  };
-  
-  export default Footer;
-  
+};
+
+export default Footer;
