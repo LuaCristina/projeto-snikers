5714e3eff30d4970042fd8d16c3de693b5145faa
Laura Cristina <lauracristina.s@hotmail.com>
2023-07-12 17:15:53 -0300

    review

--- src/componentes/section/testemonials/index.jsx
+++ src/componentes/section/testemonials/index.jsx
@@ -6,9 +6,20 @@ const Testimonials = (props) => {
       <div className="testimonials-container">
         {props.data.map((testimonial, index) => (
           <div className="testimonial" key={index}>
+            <img src={testimonial.url}></img>
             <h3>{testimonial.name}</h3>
             <p>{testimonial.content}</p>
-            <div className="rate">{testimonial.rate} estrelas</div>
+            <div className="rate">
+              {
+                (() => {
+                  let td = [];
+                  for (let i = 1; i <= testimonial.rate; i++) {
+                    td.push(<td key={i}>⭐</td>);
+                  }
+                  return td;
+                })()
+              }
+            </div> 
           </div>
         ))}
       </div>
